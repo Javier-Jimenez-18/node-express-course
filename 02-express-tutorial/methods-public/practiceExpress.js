98536bf10f889381f6bef0bbca6d7d69125a8a19
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2022-07-07 13:09:49 -0700

    Answers for assignment Week4.

--- 02-express-tutorial/methods-public/practiceExpress.js
+++ 02-express-tutorial/methods-public/practiceExpress.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const consoleLog = require('./methods-public/practice-middleware');
 
 // setup static and middleware
 app.use(express.static('./new-public'));
+app.use(consoleLog);
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './new-public/index.html'));
